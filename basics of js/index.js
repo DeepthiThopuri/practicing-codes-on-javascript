0e12a90424a42ef7709c9001b2a4b1408acb8e4d
Deepthi <topurideepthi@gmail.com>
2025-08-11 23:19:43 +0530

    Add files via upload
    This is the javascript code on basics like variables,datatypes,conditionals,looping statements

---
+++ basics of js/index.js
@@ -0,0 +1,39 @@
+console.log("hello world");
+console.log("fwomskl");
+//variables
+let num=1;
+let num1=3
+let num3=6
+let num2=3.12;
+let c="hi";
+let b=true;
+//datatypes
+let f=Number(b);
+let g=String(num2)
+let v=c+g;
+console.log(num);
+//checking type of datatype
+console.log(typeof(num))
+console.log(typeof(num2))
+console.log(typeof(c));
+console.log(typeof(b));
+console.log(typeof(f));
+console.log(typeof(g));
+console.log(v);
+//conditional statements
+if((num1+num2)==num3){
+    console.log("adding of num1 and num2 are equal to nu 3");
+}
+else{
+    console.log("adding of num1 and num2 are not equal");
+}
+//looping
+for(let i=0;i<num3;i++){
+    console.log(i)
+}
+//while  loop
+let sum=0;
+while(num1!=0){
+    sum=sum+num1;
+    num1--;
+}
