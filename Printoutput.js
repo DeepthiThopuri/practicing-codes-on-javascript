3ae362a3cacf4d3d76c2e24db323868caa9b3274
Deepthi <topurideepthi@gmail.com>
2025-08-22 18:44:03 +0530

    Add files via upload
    JavaScript printing output

---
+++ Printoutput.js
@@ -0,0 +1,7 @@
+/*Let us output a number in JavaScript.
+
+Some code has been populated in the code editor on the right.
+This code just prints 12 on the screen when you run it.*/
+console.log(12);
+console.log("I love JavaScript");
+console.log(21 + 40);
