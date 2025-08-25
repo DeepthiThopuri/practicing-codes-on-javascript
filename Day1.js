a47e988fad5698a53bea605d07080df300428aa0
Deepthi <topurideepthi@gmail.com>
2025-08-25 19:57:17 +0530

    Add files via upload
    variables and datatypes

---
+++ Day1.js
@@ -0,0 +1,11 @@
+//variable and Data types
+/*A variable is like a labelled box where you can store data. 
+Imagine you have a box labelled "age" and you put the number 25 in it. 
+ In JavaScript, you would do this by writing var age = 25; (one of the ways, we'll learn about rest soon). T
+    his means you're storing the number 25 in the variable named age.*/
+    var age = 25;
+console.log(age);
+const str="deepthi"
+console.log(str);
+let val=2.4
+console.log(val);
