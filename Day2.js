e4a0039f2c23982374d9c38ceb4b894443e7b0e1
Deepthi <topurideepthi@gmail.com>
2025-08-28 18:39:19 +0530

    Add files via upload
    We use conditions in code for decision-making and controlling the flow of a program.
    
    The if-else statement in JavaScript is used to make decisions in your code. It allows your program to execute different blocks of code depending on whether a given condition is true or false.

---
+++ Day2.js
@@ -0,0 +1,19 @@
+//conditional statements
+
+var age = 25;
+var votingAge = 18;
+
+if(age >= votingAge) {
+  console.log("Old enough to vote!");
+}
+else {
+  console.log("Not old enough to vote.");
+}
+// Another example
+let age = 18;
+
+if (age >= 13) {
+  console.log("You're a teenager!");
+} else {
+  console.log("Hello baby!");
+}
